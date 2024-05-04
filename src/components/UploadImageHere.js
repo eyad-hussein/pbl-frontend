8db0e0dbaa180d4b734c2c4f3e389225716e7c09
OmarAly03 <omar.aly@ejust.edu.eg>
2024-05-04 12:49:38 +0300

    Adjusted the Upload Buttons to Access files

--- src/components/UploadImageHere.js
+++ src/components/UploadImageHere.js
@@ -4,8 +4,11 @@ import Icon from '../components/Icon';
 function UploadImageHere() {
   return (
     <div className="uploadimagehere">
+      <input type='file' id='file' accept='image/*' style={{display: 'none'}}/>
+      <label htmlFor='file' style={{cursor: 'pointer'}}>
       <Icon className='upload' name='upload'/>
       <p className='uploadimagehere-text'>Upload Image Here</p> 
+      </label>
     </div>
   );
 }
